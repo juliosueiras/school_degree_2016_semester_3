4df72c39bd5c4ab89d0dfd31ea3fc096412464ec
Julio Tain Sueiras <juliosueiras@gmail.com>
2016-09-21 12:31:34 -0400

    Added basic espresso test

--- assignments/tip_calculator/src/androidTest/java/com/juliosueiras/tip_calculator/MainActivityTest.java
+++ assignments/tip_calculator/src/androidTest/java/com/juliosueiras/tip_calculator/MainActivityTest.java
@@ -1,21 +1,34 @@
 package com.juliosueiras.tip_calculator;
 
-import android.test.ActivityInstrumentationTestCase2;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class com.juliosueiras.tip_calculator.MainActivityTest \
- * com.juliosueiras.tip_calculator.tests/android.test.InstrumentationTestRunner
- */
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+import android.support.test.rule.ActivityTestRule;
 
-    public MainActivityTest() {
-        super("com.juliosueiras.tip_calculator", MainActivity.class);
+import android.support.test.runner.AndroidJUnit4;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.Espresso.onView;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withId(R.id.button_calculate)).perform(click());
+        onView(withId(R.id.button_clear)).perform(click());
     }
-
 }
